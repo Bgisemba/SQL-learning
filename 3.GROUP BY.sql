0f500b325aefd8c4d770dfb317d6a7903a076733
Brian Gisemba Michira <82797490+Bgisemba@users.noreply.github.com>
2025-01-09 14:59:44 +0300

    Add files via upload

---
+++ 3.GROUP BY.sql
@@ -0,0 +1,32 @@
+-- Group By
+
+SELECT gender
+FROM employee_demographics
+GROUP BY gender
+;
+
+SELECT first_name -- non aggregated column
+FROM employee_demographics
+GROUP BY gender
+;
+
+SELECT gender,AVG(age)
+FROM employee_demographics
+GROUP BY gender
+;
+
+SELECT occupation,salary
+FROM employee_salary
+GROUP BY occupation,salary
+;
+
+SELECT gender,AVG(age),MAX(age),MIN(age),COUNT(age)
+FROM employee_demographics
+GROUP BY gender
+;
+
+
+
+
+
+
